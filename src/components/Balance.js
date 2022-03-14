ee1798043cbbec942c933e6ae72f55de9b162223
Virenk21 <viren.d222111@gmail.com>
2022-03-14 22:20:08 +0530

    further updates

--- src/components/Balance.js
+++ src/components/Balance.js
@@ -1,10 +1,10 @@
-import React from 'react'
+import React from "react";
 
 export const Balance = () => {
   return (
     <>
       <h4>Your Balance</h4>
-      <h1 id="balance">$0.00</h1>
+      <h1 id="balance">₹5.97</h1>
     </>
   );
-}
+};

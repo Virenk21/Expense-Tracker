db8f74cbb09f3ec580e0966366b57bf27a32aee8
Virenk21 <viren.d222111@gmail.com>
2022-03-15 04:58:43 +0530

    added transaction file

--- src/components/TransactionList.js
+++ src/components/TransactionList.js
@@ -1,4 +1,6 @@
 import React, { useContext } from "react";
+import { Transaction } from "./Transaction";
+
 import { GlobalContext } from "../context/GlobalState";
 
 export const TransactionList = () => {
@@ -7,10 +9,8 @@ export const TransactionList = () => {
     <>
       <h3>History</h3>
       <ul className="list">
-        {transactions.map((transaction) => (<li className="minus">
-          {transaction.text} <span>-$400</span><button className="delete-btn">x</button>
-        </li> ))}
-      </ul>
+        {transactions.map((transaction) => (<Transaction key={transaction.id} transaction={transaction} />))}
+        </ul>
     </>
   );
 };

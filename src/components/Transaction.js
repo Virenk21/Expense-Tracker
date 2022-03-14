a0b81c174c06cbbc0c7ba9c1adeb7fd4fab4a370
Virenk21 <viren.d222111@gmail.com>
2022-03-15 05:13:52 +0530

    added income nd expense

--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -5,9 +5,12 @@ export const Transaction = ({ transaction }) => {
 const sign = transaction.amount < 0 ? '-' : '+';
 
 return (
-    <li className="minus">
-      {transaction.text} <span>{sign}${Math.abs(transaction.amount)}</span><button
-      className="delete-btn">x</button>
-    </li>
-  );
+  <li className={transaction.amount < 0 ? "minus" : "plus"}>
+    {transaction.text}{" "}
+    <span>
+      {sign}${Math.abs(transaction.amount)}
+    </span>
+    <button className="delete-btn">x</button>
+  </li>
+);
 }

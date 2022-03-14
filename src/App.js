397e9f207cdf545bf2cbd6bd3254a3db4e80f8dd
Virenk21 <viren.d222111@gmail.com>
2022-03-15 04:37:14 +0530

    added few more files

--- src/App.js
+++ src/App.js
@@ -6,9 +6,11 @@ import { TransactionList } from "./components/TransactionList";
 import { AddTransaction } from "./components/AddTransaction";
 import './App.css';
 
+import { GlobalProvider } from "./context/GlobalState";
+
 function App() {
   return (
-    <div>
+      <GlobalProvider>
       <Header />
       <div className="container">
         <Balance />
@@ -16,7 +18,7 @@ function App() {
         <TransactionList />
         <AddTransaction />
       </div>
-    </div>
+      </GlobalProvider>
   );
 }
 

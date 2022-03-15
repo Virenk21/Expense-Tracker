7443f234a04c62df0c7e6b006db94850bc8193e8
Virenk21 <viren.d222111@gmail.com>
2022-03-15 05:51:28 +0530

    updated globalstate

--- src/context/GlobalState.js
+++ src/context/GlobalState.js
@@ -6,12 +6,7 @@ export const GlobalState = () => {
 };
 // Initial state
 const initialState = {
-  transactions: [
-  { id: 1, text: 'Flower', amount: -20 },
-  { id: 2, text: 'Salary', amount: 300 },
-  { id: 3, text: 'Book', amount: -10 },
-  { id: 4, text: 'Camera', amount: 150 }
-]
+  transactions: []
 }
 
 // Create context

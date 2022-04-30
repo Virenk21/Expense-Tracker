10426def12b3d697164111ccb700f6ec2a1823ac
Virenk21 <viren.d222111@gmail.com>
2022-05-01 03:46:07 +0530

    updated header

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,6 +2,10 @@ import React from 'react'
 
 export const Header = () => {
   return (
-    <h2>Expense Tracker</h2>
-  )
+    <h2>
+      Expense Tracker for (Kinnu my love{" "}
+      <img src="https://img.icons8.com/tiny-color/16/000000/experimental-like-tiny-color.png" />
+      )
+    </h2>
+  );
 }
